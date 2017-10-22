62ab7010aa932ab7b8cb753252c81159575a6583
kloontim <timon.gronotte@hotmail.de>
2017-10-23 00:33:39 +0200

    added some comments

--- software/raspberrypi_qt/mainwindow.h
+++ software/raspberrypi_qt/mainwindow.h
@@ -1,33 +1,31 @@
 #ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#define MAINWINDOW_H//Standart Header
 
-#include <QMainWindow>
+#include <QMainWindow>//einbeziehen von QT
 #include <QImage>
 
-class QLabel;
+class QLabel;//erstellen von Classen
 class LeptonThread;
 class QGridLayout;
 
-class MainWindow : public QMainWindow {
-    Q_OBJECT
-
-    enum { ImageWidth = 320, ImageHeight = 240 };
+class MainWindow : public QMainWindow {//Erstellen von Main Window als kind von Q Main Window
+    Q_OBJECT enum { ImageWidth = 320, ImageHeight = 240 };//scheint größe festzulegen
 
     static int snapshotCount;
 
-    QLabel *imageLabel;
+    QLabel *imageLabel;//den erstellten Classen die tatsächliche Klasse zuweisen, da nur Pointer
     LeptonThread *thread;
     QGridLayout *layout;
 
-    unsigned short rawMin, rawMax;
+    unsigned short rawMin, rawMax;//erstellen der Werte für das Rendern eines Live bildes
     QVector<unsigned short> rawData;
     QImage rgbImage;
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget *parent = 0);//killt vermutlich start Fenster umn öffnet neues
 
 public slots:
-    void saveSnapshot();
+    void saveSnapshot();//provides functions for functionality
     void updateImage(unsigned short *, int, int);
 };
 
